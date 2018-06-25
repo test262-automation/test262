c2aa3a31e7c3067c3f4cab14c2c2724b0be27907
bmac <bmac325@gmail.com>
2018-06-25 15:21:26 -0400

    Partially modify a file that will not be modified in the vendor repo

--- implementation-contributed/javascriptcore/262-partially-curated-vendor-not-modified/empty-function.js
+++ implementation-contributed/javascriptcore/262-partially-curated-vendor-not-modified/empty-function.js
@@ -1,10 +1,12 @@
-function foo() { }
+function bar() { }
 
-noInline(foo);
+noInline(bar);
 
 for (var i = 0; i < 100000; ++i) {
-    var result = foo();
+    var result = bar();
     if (result !== void 0)
         throw "You broke JSC so hard that even the empty function doesn't work: " + result;
 }
+
+// partially modify empty-function.js
 
