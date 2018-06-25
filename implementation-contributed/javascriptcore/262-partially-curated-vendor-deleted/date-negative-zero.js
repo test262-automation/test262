68edf514c38fccf57aa86a94b1e0b1a0ef150b86
bmac <bmac325@gmail.com>
2018-06-25 15:18:26 -0400

    Partially modify a file that will be deleted in the vendor repo

--- implementation-contributed/javascriptcore/262-partially-curated-vendor-deleted/date-negative-zero.js
+++ implementation-contributed/javascriptcore/262-partially-curated-vendor-deleted/date-negative-zero.js
@@ -3,5 +3,4 @@ function shouldBe(actual, expected) {
         throw new Error('bad value: ' + actual);
 }
 
-var date = new Date(-0);
-shouldBe(Object.is(date.getTime(), 0), true);
+// Partially curate date-negative-zero.js
