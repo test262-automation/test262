ac42b851a695bce7aaa7c6fb16cb50b49d27c51c
bmac <bmac325@gmail.com>
2018-06-27 13:55:33 -0400

    [IMPLEMENTATION-PREFIX] changes from source at sha de9f990b on 1530122120638

--- implementation-contributed/javascriptcore/APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION/has-own-property-called-on-non-object.js
+++ implementation-contributed/javascriptcore/APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION/has-own-property-called-on-non-object.js
@@ -1,24 +1,8 @@
-"use strict";
 
-let objs = [
-    function() { },
-    new String(),
-    {foo: 45},
-    {bar:50, foo: 45},
-    {baz:70, bar:50, foo: 45},
-    new Date,
-];
-
-let has = ({}).hasOwnProperty;
-function foo(o) {
-    return has.call(o, "foo");
-}
-
-// Partially curate has-own-property-called-on-non-object.js
-
-noInline(foo);
-
-for (let i = 0; i < 10000; i++)
-    foo(objs[i % objs.length]);
-
-foo("foo");
+    /*
+    ****************************************************** test262-automation ******************************************************
+    Summary: Source file deleted after partial curation.
+        File Status: Partially curated & modified.
+        Source Status: Deleted since export.
+        This message was added on undefined
+    */
