aa8c3e245e0a7aca4a83d1b696e74d5eac56e495
Travis CI User <travis@example.org>
2018-06-29 18:35:44 +0000

    [IMPLEMENTATION-PREFIX] changes from source at sha 1a4026b7 on 1530297341356

--- implementation-contributed/javascriptcore/262-partially-curated-vendor-deleted/date-negative-zero.js
+++ implementation-contributed/javascriptcore/262-partially-curated-vendor-deleted/date-negative-zero.js
@@ -1,6 +1,8 @@
-function shouldBe(actual, expected) {
-    if (actual !== expected)
-        throw new Error('bad value: ' + actual);
-}
 
-// Partially curate date-negative-zero.js
+    /*
+    ****************************************************** test262-automation ******************************************************
+    Summary: Source file deleted after partial curation.
+        File Status: Partially curated & modified.
+        Source Status: Deleted since export.
+        This message was added on undefined
+    */
